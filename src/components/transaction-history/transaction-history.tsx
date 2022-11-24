a66ace67836fb163a1d064ed7f1effa351082724
Ilya Katts <ilyak@wix.com>
2022-11-24 10:25:29 +0200

    Fix label

--- src/components/transaction-history/transaction-history.tsx
+++ src/components/transaction-history/transaction-history.tsx
@@ -51,7 +51,7 @@ export const TransactionHistory = () => {
       <Card.Title>Transaction History</Card.Title>
       <Card.Content>
         <div className={styles.legend}>
-          <span className={styles.receiver}>receiver</span>
+          <span className={styles.receiver}>Receiver</span>
           <span className={styles.type}>Type</span>
           <span className={styles.date}>Date</span>
           <span className={styles.amount}>Amount</span>
