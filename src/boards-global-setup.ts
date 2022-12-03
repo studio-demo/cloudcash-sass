a7756efbacc794931a99f7ab7c095f9ff060577a
Ilya Katts <ilyak@wix.com>
2022-12-03 14:52:39 +0200

    Fix global setup tokens import

--- src/boards-global-setup.ts
+++ src/boards-global-setup.ts
@@ -1,5 +1,2 @@
 import "./styles/globals.scss";
-import "./styles/_color.scss";
-import "./styles/_radius.scss";
-import "./styles/_spacing.scss";
-import "./styles/_type.scss";
+import "./styles/_tokens.scss";
